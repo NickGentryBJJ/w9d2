62500fb2929d21afa4fddcfa74b372ddd689f86e
Shanna Le <shannatle@gmail.com>
2023-05-23 13:34:17 -0700

    Finish set up

---
+++ practice-for-ch-js-browser-ttt-main 2/src/index.js
@@ -0,0 +1,12 @@
+// Add your import statements for View and Game here
+
+
+const View = require("./ttt-view.js");
+const Game = require("../ttt_node/game.js");
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Your code here
+});
+
+// const newGame = new Game()
